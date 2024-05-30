adaa3ee878fd01340431bd1cb370704419d77095
thedevabror <abrorkhandeveloper@gmail.com>
2024-05-30 17:44:46 +0500

    com 9

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // Foydalanuvchi buyurtma yaratishi
 router.post("/:userId", protect, createOrder);
 router.get("/:userId", protect, getUserOrders);
-router.get("/:orderId", protect, getOrderById);
+router.get("/details/:orderId", protect, getOrderById);
 router.get("/", protect, admin, getAllOrders);
 router.put("/update-order-status", protect, admin, updateOrderStatus);
 
