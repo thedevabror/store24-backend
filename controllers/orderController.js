f0a215ace1e096bf537318bf19b3c1b1b05563cf
thedevabror <abrorkhandeveloper@gmail.com>
2024-05-30 19:52:43 +0500

    update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -118,13 +118,11 @@ const deleteOrder = async (req, res) => {
   try {
     const orderId = req.params.orderId;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findByIdAndDelete(orderId);
 
     if (!order) {
       return res.status(404).json({ message: "Order not found" });
     }
-
-    await order.remove();
 
     res.status(200).json({ message: "Order deleted successfully" });
   } catch (error) {
